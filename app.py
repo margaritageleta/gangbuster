847c1e2e4fd05c0ff7262010a2df9fbbc0c4da56
Rita <rita.geleta@jediupc.com>
2021-03-30 00:30:39 +0200

    Gangbuster app

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template as render_template, session, redirect, url_for
+
+# Set up App
+app = Flask(__name__, 
+            static_url_path = '', 
+            static_folder = 'static',
+            template_folder = 'templates')
+
+# Flask App configuration
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# Root: main.html
+@app.route('/')
+def home():
+    return render_template('base.html')
+@app.route('/page')
+def page():
+    return render_template('page.html')
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.run(debug = False, host = '127.0.0.1')
